f6965af113c1d160f8259b5fe164961db51393c4
Huynh-Nguyen <huynhng30490@gmail.com>
2020-10-03 22:23:09 -0700

    third commit

--- backup.sh
+++ backup.sh
@@ -15,13 +15,30 @@ function total_files {
 function total_directories {
 	find $1 -type d | wc -l
 }
+function total_archived_directories {
+	tar -tzf $1 | grep  /$ | wc -l
+}
+function total_archived_files {
+	tar -tzf $1 | grep -v /$ | wc -l
+}
 
 tar -czf $output $input 2> /dev/null
 
-echo -n "Files to be included:" 
-total_files $input
-echo -n "Directories to be included :" 
-total_directories $input
+src_files=$( total_files $input )
+src_directories=$( total_directories $input )
+
+arch_files=$( total_archived_files $output )
+arch_directories=$( total_archived_directories $output )
+
+echo "Files to be included: $src_files" 
+echo "Directories to be included : $src_directories" 
+echo "Files archived: $arch_files"
+echo "Directories archived: $arch_directories"
+
+if [ $src_files -eq $arch_files ]; then
         
-echo "Backup of $input completed! Details about the output backup file:"
-ls -1 $output
+	echo "Backup of $input completed! Details about the output backup file:"
+	ls -1 $output
+else 
+	echo "Backup of $input failed!"
+fi
