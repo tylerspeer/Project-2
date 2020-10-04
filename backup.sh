d6952c675516235b822aa8148f95a403570692a9
Huynh-Nguyen <huynhng30490@gmail.com>
2020-10-03 21:54:13 -0700

    first commit

--- backup.sh
+++ backup.sh
@@ -1,3 +1,12 @@
 #!/usr/bin/bash
-`
+
 tar -czf /tmp/myhome_directory.tar.gz /c/users/huynh304/Project-2/
+#This bash script is used to backup a user's home directory to /tmp/.
+
+user=$(whoami)
+input=/home/$user
+output=/tmp/${user}_home_$(date +%Y-%m-%d_%H%M%S).tar.gz
+
+tar -czf $output $input
+echo "Backup of $input completed! Details about the output backup file:"
+ls -1 $output
